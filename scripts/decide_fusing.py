5068c3bc64f4431e863472226df504aad67f56b7
Rafael Stahl <r.stahl@tum.de>
2020-03-27 11:52:24 +0100

    ilp_script: update to use data from paper

--- scripts/decide_fusing.py
+++ scripts/decide_fusing.py
@@ -2,13 +2,13 @@ from __future__ import print_function
 from ortools.linear_solver import pywraplp
 
 # Number of nodes.
-N = 2
-# Number of layers.
-L = 4
-# Layer input sizes.
-M = [4, 8, 16, 4]
-# Layer output sizes.
-K = [8, 16, 4, 4]
+N = 6
+# Layer input/output sizes.
+#S = [4, 8, 16, 4, 4]
+S = [38*38*256, 38*38*512, 19*19*512, 19*19*1024, 19*19*512, 19*19*1024, 19*19*512, 19*19*1024, 19*19*1024, 19*19*1024, 38*38*512, 38*38*64, 19*19*256, 19*19*1280, 19*19*1024, 19*19*425, 0]
+# Convolutional layers.
+#CONV = [1, 1, 1, 1]
+CONV = [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0]
 
 def getC_LOP(l):
   return M[l] - 0
@@ -51,13 +51,19 @@ def solve():
   solveType = pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING
   solver = pywraplp.Solver("deepthings_solver", solveType)
 
+  if len(S) != len(CONV)+1:
+    print("length of S and CONV do not match")
+    exit(1)
+
+  L = len(CONV)
+
   # Create variables.
   o = []
   i = []
   f = []
   s = []
   r = []
-  oc1 = []
+  oc = []
   fc = []
   for l in range(0, L):
     o.append(solver.BoolVar('o' + str(l)))
@@ -65,8 +71,9 @@ def solve():
     f.append(solver.BoolVar('f' + str(l)))
     s.append(solver.BoolVar('s' + str(l)))
     r.append(solver.BoolVar('r' + str(l)))
-    oc1.append(solver.BoolVar('oc1' + str(l)))
-    fc.append(solver.BoolVar('fc' + str(l)))
+
+    notconv = solver.BoolVar('notconv' + str(l))
+    forceTo(solver, notconv, not CONV[l])
 
     # A layer can only be of one layer type.
     ct1 = solver.Constraint(1, 1, 'ct1' + str(l))
@@ -74,6 +81,7 @@ def solve():
     ct1.SetCoefficient(i[l], 1)
     ct1.SetCoefficient(f[l], 1)
     ct1.SetCoefficient(s[l], 1)
+    ct1.SetCoefficient(notconv, 1)
 
     # After a first fused layer, the next layer must be the second fused one.
     if l != 0:
@@ -95,11 +103,13 @@ def solve():
 
     # Multiplied coefficients for some LOP and FUSE1 terms.
     if l == 0:
-      forceTo(solver, oc1[l], 0)
+      oc.append(solver.BoolVar('oc' + str(l)))
+      fc.append(solver.BoolVar('fc' + str(l)))
+      forceTo(solver, oc[l], 0)
       forceTo(solver, fc[l], 0)
     else:
-      oc1[l] = makeMultConstraint(solver, o[l], r[l-1], 'oc1')
-      fc[l] = makeMultConstraint(solver, f[l], r[l-1], 'fc')
+      oc.append(makeMultConstraint(solver, o[l], r[l-1], 'oc'))
+      fc.append(makeMultConstraint(solver, f[l], r[l-1], 'fc'))
 
 
   # Last layer cannot be first fused one.
@@ -108,12 +118,32 @@ def solve():
   # First layer cannot be second fused one.
   forceTo(solver, s[0], 0)
 
+  # Test alternatives.
+  '''
+  forceTo(solver, o[0], 1)
+  forceTo(solver, f[2], 1)
+  forceTo(solver, s[3], 1)
+  forceTo(solver, f[4], 1)
+  forceTo(solver, s[5], 1)
+  forceTo(solver, o[6], 1)
+  forceTo(solver, f[7], 1)
+  forceTo(solver, s[8], 1)
+  forceTo(solver, i[10], 1)
+  forceTo(solver, f[13], 1)
+  forceTo(solver, s[14], 1)
+  '''
+
+  # Layer input sizes.
+  M = S[:-1]
+  # Layer output sizes.
+  K = S[1:]
+
   obj = solver.Objective()
   obj.SetMinimization()
   for l in range(0, L):
     # LOP
     obj.SetCoefficient(o[l], M[l] * (N-1) + (K[l]*N - K[l]) / N)
-    obj.SetCoefficient(oc1[l], -(M[l] * (N-1)))
+    obj.SetCoefficient(oc[l], -(M[l] * (N-1)))
     obj.SetCoefficient(r[l], K[l] * (N-1) - ((K[l]*N - K[l]) / N))
     # LIP
     obj.SetCoefficient(i[l], (M[l]*N - M[l]) / N + K[l] * (N-1))
@@ -126,15 +156,20 @@ def solve():
 
   solver.Solve()
   print('Total C: ', obj.Value())
+  print("Communication size:", "{:,}".format(4*int(obj.Value())), "Bytes")
   for l in range(0, L):
-    print('---')
-    print('o' + str(l) + ' = ' + str(o[l].solution_value()))
-    print('oc1' + str(l) + ' = ' + str(oc1[l].solution_value()))
-    print('i' + str(l) + ' = ' + str(i[l].solution_value()))
-    print('f' + str(l) + ' = ' + str(f[l].solution_value()))
-    print('fc' + str(l) + ' = ' + str(fc[l].solution_value()))
-    print('s' + str(l) + ' = ' + str(s[l].solution_value()))
-    print('r' + str(l) + ' = ' + str(r[l].solution_value()))
+    def getLayerType():
+      if o[l].solution_value():
+        return "LOP" + (" with reuse" if r[l].solution_value() else "")
+      elif i[l].solution_value():
+        return "LIP"
+      elif f[l].solution_value():
+        return "FUSE1"
+      elif s[l].solution_value():
+        return "FUSE2"
+      else:
+        return "Not convolutional"
+    print("Layer", l, "is:", getLayerType())
 
 
 if __name__ == "__main__":
