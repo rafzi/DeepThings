706b76afd66747bc9c66b88b67101c121952813c
Rafael Stahl <r.stahl@tum.de>
2020-04-06 11:58:53 +0200

    improve command line

--- src/top.c
+++ src/top.c
@@ -26,33 +26,41 @@
 static const char* addr_list[MAX_EDGE_NUM] = EDGE_ADDR_LIST;
 
 int main(int argc, char **argv){
-   uint32_t this_cli_id = 0;
-
    uint32_t partitions_h = get_int_arg(argc, argv, "-n", 5);
    uint32_t partitions_w = get_int_arg(argc, argv, "-m", 5);
    uint32_t fused_layers = get_int_arg(argc, argv, "-l", 16);
-   uint32_t total_cli_num = get_int_arg(argc, argv, "-total_edge", 0);
+   uint32_t total_cli_num = get_int_arg(argc, argv, "-total_edge", 1);
+   uint32_t this_cli_id = get_int_arg(argc, argv, "-edge_id", 0);
+   char *mode = get_string_arg(argc, argv, "-mode", "none");
+   char *network_file = get_string_arg(argc, argv, "-net", "models/yolo.cfg");
+   char *weight_file = get_string_arg(argc, argv, "-w", "models/yolo.weights");
 
-   char network_file[30] = "models/yolo.cfg";
-   char weight_file[30] = "models/yolo.weights";
-
-   if(0 == strcmp(get_string_arg(argc, argv, "-mode", "none"), "start")){
+   if(0 == strcmp(mode, "start")){
       printf("start\n");
       exec_start_gateway(START_CTRL, TCP, GATEWAY_PUBLIC_ADDR);
-   }else if(0 == strcmp(get_string_arg(argc, argv, "-mode", "none"), "gateway")){
+   }else if(0 == strcmp(mode, "gateway")){
       printf("Gateway device\n");
       printf("We have %d edge devices now\n", get_int_arg(argc, argv, "-total_edge", 0));
       deepthings_gateway(partitions_h, partitions_w, fused_layers, network_file, weight_file, total_cli_num, addr_list);
-   }else if(0 == strcmp(get_string_arg(argc, argv, "-mode", "none"), "data_src")){
+   }else if(0 == strcmp(mode, "data_src")){
       printf("Data source edge device\n");
-      printf("This client ID is %d\n", get_int_arg(argc, argv, "-edge_id", 0));
-      this_cli_id = get_int_arg(argc, argv, "-edge_id", 0);
+      printf("This client ID is %d\n", this_cli_id);
       deepthings_victim_edge(partitions_h, partitions_w, fused_layers, network_file, weight_file, this_cli_id, total_cli_num, addr_list);
-   }else if(0 == strcmp(get_string_arg(argc, argv, "-mode", "none"), "non_data_src")){
+   }else if(0 == strcmp(mode, "non_data_src")){
       printf("Idle edge device\n");
-      printf("This client ID is %d\n", get_int_arg(argc, argv, "-edge_id", 0));
-      this_cli_id = get_int_arg(argc, argv, "-edge_id", 0);
+      printf("This client ID is %d\n", this_cli_id);
       deepthings_stealer_edge(partitions_h, partitions_w, fused_layers, network_file, weight_file, this_cli_id, total_cli_num, addr_list);
+   }else{
+      printf("Usage: %s [options]\n"
+      "  -n          FTP partitions in height dimension\n"
+      "  -m          FTP partitions in width dimension\n"
+      "  -l          Number of layers that should use FTP\n"
+      "  -total_edge Number of edge devices\n"
+      "  -edge_id    The edge ID for this instance\n"
+      "  -mode       The mode to use: \"gateway\", \"data_src\", \"non_data_src\", \"start\"\n"
+      "  -net        The network structure file to use (.cfg)\n"
+      "  -w          The weights file to use (.weights)\n", argv[0]);
+      return 1;
    }
    return 0;
 }
